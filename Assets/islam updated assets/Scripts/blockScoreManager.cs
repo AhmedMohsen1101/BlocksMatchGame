240c791d2b3fa29f866ca0d438329221c55c3487
islam dya <49913188+dukeiso199@users.noreply.github.com>
2020-02-08 14:29:33 +0200

    task of palcement and score

---
+++ Assets/islam updated assets/Scripts/blockScoreManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class blockScoreManager : MonoBehaviour
+{
+    public int score = 0;
+    public Text blocksscored;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Tile")
+        {
+            score++;
+            blocksscored.text = "" + score;
+        }
+    }
+}
